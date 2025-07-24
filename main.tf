cae8068d3971badbde1eadd0c73e20ec6fff18b1
Alex13-th <aleks.shaparenko@gmail.com>
2025-07-24 13:16:43 +0200

    Solution

--- main.tf
+++ main.tf
@@ -1,28 +1,48 @@
-# Uncomment the ami datasource to get the AMI id for your ec2 instance: 
-# data "aws_ami" "this" {
-#   most_recent = true
-#   owners      = ["amazon"]
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
 
-#   filter {
-#     name = "name"
-#     values = ["amzn2-ami-hvm-*-gp2"]
-#   }
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
 
-#   filter {
-#     name   = "root-device-type"
-#     values = ["ebs"]
-#   }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   filter {
-#     name   = "architecture"
-#     values = ["x86_64"]
-#   }
-# }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
 
 # create a key pair 
+resource "aws_key_pair" "deploy_key" {
+  key_name   = "deployer-key"
+  public_key = file(var.public_key_path)
+}
+
+
 # create an ec2 instance 
+
+resource "aws_instance" "this" {
+  ami           = data.aws_ami.this.id
+  instance_type = "t3.micro"   # У регіоні (eu-north-1) інстанс типу t2.micro більше не входить у Free Tier
+  associate_public_ip_address = true
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name = aws_key_pair.deploy_key.key_name
+  user_data = file(var.script_parh)
+
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,15 @@ variable "security_group_id" {
     type = string
     description = "ID of the security group, you deployed in the previous task."
 }
+
+variable "public_key_path" {
+  description = "My SSH key path "
+  type        = string
+  default     = "~/.ssh/id_ed25519.pub"
+}
+
+variable "script_parh" {
+  description = "My grafana install script path"
+  type        = string
+  default     = "./install-grafana.sh"
+}
